073af217337e08b8589e5c485cf2284882c8a70f
Barbosa <pbarboje@myuax.com>
2024-09-24 13:39:17 +0200

    PP

---
+++ security-system/src/main/java/com/starkindustries/security_system/SecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.starkindustries.security_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySystemApplication.class, args);
+	}
+
+}
---
+++ security-system/src/test/java/com/starkindustries/security_system/SecuritySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starkindustries.security_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
